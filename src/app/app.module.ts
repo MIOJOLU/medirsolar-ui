fb5b8352b0a2d3375832b3a73d3f3d03e2111fee
MIOJOLU <64047127+MIOJOLU@users.noreply.github.com>
2022-05-26 09:01:24 -0300

    first commit: add arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { MedicaoComponent } from './components/medicao/medicao.component';
+import { EstatisticaComponent } from './components/estatistica/estatistica.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MedicaoComponent,
+    EstatisticaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: 'home', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
